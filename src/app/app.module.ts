ae9022f375e94586f912727d27795a481c6fb6b2
Chauhan Kirankumar <kirankumar.chauhan@tieto.com>
2019-06-17 16:58:35 +0530

    Firebase Hosting Settings Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [MyClientDataService],
   exports: [RouterModule],
